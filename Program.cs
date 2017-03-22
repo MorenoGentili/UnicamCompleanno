4488fe8c64a7db49d0f9a7009d6c9e2ab581d43e
Moreno Gentili <moreno@smartmobile.it>
2017-03-22 20:15:46 +0100

    Prima versione del programma

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace UnicamCompleanno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Questo programma ti dice la tua eta' e il numero di giorni che restano al tuo compleanno");
+            Console.WriteLine("Digita la tua data di nascita: ");
+            string testoDigitatoDallUtente = Console.ReadLine();
+
+            try {
+                DateTime dataDiNascita = DateTime.Parse(testoDigitatoDallUtente);
+
+                //Calcoliamo gli anni dell'utente.
+                //Quando non sappiamo come si fa o quando, come in questo caso,
+                //.NET non ha un modo facile per ottenere la differenza in anni
+                //andiamo a vedere su StackOverlow se qualcuno ha già avuto la stessa necessità
+                //Il metodo Years è stato preso da lì (è definito più in basso, in questo file)
+                string anniDiDifferenza = Years(dataDiNascita, DateTime.Today);
+                Console.WriteLine($"Oggi hai {anniDiDifferenza} anni");
+
+                //Ora calcoliamo i giorni che ci separano dal prossimo compleanno
+                DateTime dataProssimoCompleanno = new DateTime(DateTime.Today.Year, dataDiNascita.Month, dataDiNascita.Day);
+                TimeSpan differenzaTemporale = dataProssimoCompleanno - DateTime.Today;
+                double differenzaInGiorni = differenzaTemporale.TotalDays;
+                Console.WriteLine($"Restano {differenzaInGiorni} giorni al tuo prossimo compleanno!");
+
+            } catch (Exception exc) {
+                Console.WriteLine($"Non hai digitato una data valida, il programma ora terminera'. L'errore e' stato: {exc.Message}.");
+            }
+            Console.ReadKey();
+        }
+
+        //Grazie, StackOverflow!
+        //http://stackoverflow.com/questions/4127363/date-difference-in-years-using-c-sharp#answer-4127477
+        static int Years(DateTime start, DateTime end)
+        {
+            return (end.Year - start.Year - 1) +
+                (((end.Month > start.Month) ||
+                ((end.Month == start.Month) && (end.Day >= start.Day))) ? 1 : 0);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # UnicamCompleanno
-Applicazione console C# che calcola l'età dell'utente e gli indica i giorni che lo separano dal suo prossimo compleanno
+Applicazione console C# che calcola l'età dell'utente e gli indica i giorni che lo separano dal suo prossimo compleanno.
+
+## Comportamento desiderato (specifica del programma)
+1. Il programma deve chiedere all'utente di digitare la propria data di nascita;
+2. La data deve essere inserita nel formato italiano `gg/mm/aaaa` oppure nel formato internazionale `aaaa/mm/gg`;
+3. L'età della persona deve essere calcolata correttamente, anche nel giorno del suo compleanno;
+4. Il numero di giorni che restano al prossimo compleanno deve essere calcolato correttamente;
+5. *Nice-to-have*: Se è il giorno del compleanno dell'utente, il programma non deve scrivere il numero di giorni che restano ma deve scrivere "Tanti auguri!".
+
+## Situazione attuale
+Il programma ha dei problemi e non è ancora stato ancora rilasciato al cliente. Al momento, non si riesce neanche a compilare il sorgente (premendo il tasto F5, Visual Studio Code dice che sono stati rilevati errori).
+Per prima cosa va risolto quello, e poi anche un altro problema: per alcune date di nascita, il programma scrive "Restano -80 giorni al tuo prossimo compleanno!".
+Come è possibile che scriva un numero negativo? Va identificato il problema e risolto. Hai giusto notato che si verifica per i nati a gennaio, febbraio e alcuni di marzo.
