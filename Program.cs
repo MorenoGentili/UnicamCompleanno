cbfd91934de981c0e7c7624f23ef1f3a151cbe4a
MorenoGentili <gentili.moreno@gmail.com>
2017-05-04 22:54:14 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ namespace UnicamCompleanno
     {
         static void Main(string[] args)
         {
+            //This program prints your age and the number of remaining days to your next birthday
             Console.WriteLine("Questo programma ti dice la tua eta' e il numero di giorni che restano al tuo compleanno");
+            //Type your date of birth
             Console.WriteLine("Digita la tua data di nascita: ");
             string testoDigitatoDallUtente = Console.ReadLine();
 
@@ -14,26 +16,27 @@ namespace UnicamCompleanno
                 DateTime dataDiNascita = DateTime.Parse(testoDigitatoDallUtente);
 
                 //Calcoliamo gli anni dell'utente.
-                //Quando non sappiamo come si fa o quando, come in questo caso,
-                //.NET non ha un modo facile per ottenere la differenza in anni
-                //andiamo a vedere su StackOverlow se qualcuno ha già avuto la stessa necessità
-                //Il metodo Years è stato preso da lì (è definito più in basso, in questo file)
+                //Calculate the age of the user
                 string anniDiDifferenza = Years(dataDiNascita, DateTime.Today);
                 Console.WriteLine($"Oggi hai {anniDiDifferenza} anni");
 
                 //Ora calcoliamo i giorni che ci separano dal prossimo compleanno
+                //Now, we calculate the remaining days to his/her next birthday
                 DateTime dataProssimoCompleanno = new DateTime(DateTime.Today.Year, dataDiNascita.Month, dataDiNascita.Day);
                 TimeSpan differenzaTemporale = dataProssimoCompleanno - DateTime.Today;
                 double differenzaInGiorni = differenzaTemporale.TotalDays;
+                //days remaining to your next birthday
                 Console.WriteLine($"Restano {differenzaInGiorni} giorni al tuo prossimo compleanno!");
 
             } catch (Exception exc) {
+                //You didn't input a valid date, the program will now end. The error was:
                 Console.WriteLine($"Non hai digitato una data valida, il programma ora terminera'. L'errore e' stato: {exc.Message}.");
             }
             Console.ReadKey();
         }
 
         //Grazie, StackOverflow!
+        //Thanks, StackOverflow!
         //http://stackoverflow.com/questions/4127363/date-difference-in-years-using-c-sharp#answer-4127477
         static int Years(DateTime start, DateTime end)
         {
